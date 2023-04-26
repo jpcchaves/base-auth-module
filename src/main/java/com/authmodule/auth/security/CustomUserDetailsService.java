90bfbf07894450ca24eba96f43866ca750f35e8a
João Paulo <jpcchaves@outlook.com>
2023-04-26 15:55:24 -0300

    feat: add custom user details service

---
+++ src/main/java/com/authmodule/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.authmodule.auth.security;
+
+import com.authmodule.auth.entities.User;
+import com.authmodule.auth.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = repository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+
+        return user;
+    }
+}
