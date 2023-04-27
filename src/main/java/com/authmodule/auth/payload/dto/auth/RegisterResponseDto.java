27b2af682a617efdb15d4a21f6265d05b859f346
João Paulo <jpcchaves@outlook.com>
2023-04-27 13:32:29 -0300

    feat: add register response and request dto

---
+++ src/main/java/com/authmodule/auth/payload/dto/auth/RegisterResponseDto.java
@@ -0,0 +1,64 @@
+package com.authmodule.auth.payload.dto.auth;
+
+public class RegisterResponseDto {
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private Boolean isAdmin;
+
+    public RegisterResponseDto() {
+    }
+
+    public RegisterResponseDto(Long id,
+                               String name,
+                               String username,
+                               String email,
+                               Boolean isAdmin) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.isAdmin = isAdmin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+}
