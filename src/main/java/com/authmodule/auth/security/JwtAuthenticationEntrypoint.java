b1bda6f852cb269da9b8b4340c098e264d1c145e
João Paulo <jpcchaves@outlook.com>
2023-04-27 12:02:27 -0300

    feat: add jwt auth entrypoints

---
+++ src/main/java/com/authmodule/auth/security/JwtAuthenticationEntrypoint.java
@@ -0,0 +1,39 @@
+package com.authmodule.auth.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class JwtAuthenticationEntrypoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse res,
+                         AuthenticationException authException) throws IOException, ServletException {
+        res.setContentType("application/json;charset=UTF-8");
+        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        // Create response content
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.accumulate("timestamp", new Date().getTime());
+            obj.accumulate("status", HttpServletResponse.SC_UNAUTHORIZED);
+            obj.accumulate("title", "Acesso Negado!");
+            obj.accumulate("message", "Você não possui autorização para acessar esse recurso!");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        res.getWriter().write(obj.toString());
+    }
+}
